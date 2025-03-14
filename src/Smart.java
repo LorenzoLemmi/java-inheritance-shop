9e133d44bb05a000dd3ea37f3be515985a7992f7
LorenzoLemmi <lorenzo.lemmi92@gmail.com>
2025-03-14 16:18:52 +0100

    feat: Add class Smartphone

---
+++ src/Smart.java
@@ -0,0 +1,9 @@
+public class Smart {
+
+    private String codiceIMEI;
+    private int quantitàDiMemoria;
+
+    public class Smarphone (int codice, String nome, String marca, Double prezzo, double iva) {
+
+    }
+}
---
+++ src/Smartphone.java
@@ -0,0 +1,31 @@
+public class Smartphone extends Prodotto {
+
+    private String codiceIMEI;
+    private int quantitaDiMemoria;
+
+    public Smartphone (int codice, String nome, String marca, Double prezzo, double iva,
+        String codiceIMEI, int quantitaDiMemoria) {
+
+            super(codice, nome, marca, prezzo, iva);
+            this.codiceIMEI = codiceIMEI;
+            this.quantitaDiMemoria = quantitaDiMemoria;
+
+            
+    }
+
+    public String getCodiceIMEI() {
+        return codiceIMEI;
+    }
+
+    public void setCodiceIMEI(String codiceIMEI) {
+        this.codiceIMEI = codiceIMEI;
+    }
+
+    public int getQuantitaDiMemoria() {
+        return quantitaDiMemoria;
+    }
+
+    public void setQuantitaDiMemoria(int quantitaDiMemoria) {
+        this.quantitaDiMemoria = quantitaDiMemoria;
+    }
+}
